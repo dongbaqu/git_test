a5e732e4acbd4752336c1f2ce44140398dd5c3da
Cwj <Cwj@cwj.com>
2022-08-29 14:22:14 +0800

    five commit

--- src/main/java/com/cwj/git/GitTest.java
+++ src/main/java/com/cwj/git/GitTest.java
@@ -6,5 +6,7 @@ package com.cwj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("test git master01......");
+        System.out.println("test git hot_03----fix......");
+        System.out.println("test git master02......");
     }
 }
