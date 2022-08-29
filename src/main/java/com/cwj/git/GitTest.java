3c7fb3f54d19c711c8a92bdc16375cde5c5405ae
Cwj <Cwj@cwj.com>
2022-08-29 13:48:07 +0800

    second commit

--- src/main/java/com/cwj/git/GitTest.java
+++ src/main/java/com/cwj/git/GitTest.java
@@ -6,5 +6,6 @@ package com.cwj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("test git ......");
+        System.out.println("test ......");
     }
 }
